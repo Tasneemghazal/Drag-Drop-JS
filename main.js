81858bb0e88cd8b3c86a3522aaebe2481cd397d8
Tasneem <tasneemgazal5@gmail.com>
2023-10-25 10:57:16 +0300

    commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let lists = document.getElementsByClassName("list");
+let right = document.getElementById("right");
+let left = document.getElementById("left");
+
+for (let i = 0; i < lists.length; i++) {
+  lists[i].addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    right.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+
+    right.addEventListener("drop", function (e) {
+      right.appendChild(selected);
+      selected = null;
+    });
+
+    left.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+
+    left.addEventListener("drop", function (e) {
+      left.appendChild(selected);
+      selected = null;
+    });
+  });
+}
